091e1d45dfeb8dccbba03ad33ceab3f1a6dde1af
Jon Levine <jonalevine1@gmail.com>
2018-04-27 11:48:12 -0400

    added test photos and cleaned up style

--- 350.6/src/edu/upenn/cis350/hwk6/Main.java
+++ 350.6/src/edu/upenn/cis350/hwk6/Main.java
@@ -1,6 +1,5 @@
 package edu.upenn.cis350.hwk6;
 
-import java.util.Arrays;
 
 public class Main {
 
--- 350.6/src/edu/upenn/cis350/hwk6/MultiBlur.java
+++ 350.6/src/edu/upenn/cis350/hwk6/MultiBlur.java
@@ -1,52 +1,60 @@
 package edu.upenn.cis350.hwk6;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.awt.Color;
-import java.util.concurrent.Callable;
+
 
 public class MultiBlur {
 	
 	private int [] [] image;
 	
-	
+
 	public MultiBlur(int [] [] image) {
 		this.image = image;
 	}
 	
-	
+
 	public int [][] blurImage(int numThreads, int blurSize) throws InterruptedException {
 		
 		int [] [] copy = new int[image.length][image[0].length];
-	
-		Thread [] threads = new Thread[numThreads];
 		int size = image.length;
+		if (numThreads > size) {
+			numThreads = size;
+		}
+		
+		Thread [] threads = new Thread[numThreads];
+		
 		int stepSize = size/numThreads;
 		int count = 0;
 		boolean exit = false;
+		
+		//Divide image into sections for each thread
 		for (int i = 0; i < size && !exit; i+=stepSize) {
 			int end = -1;
+			
+			// Deals with size not divisible by numThreads
 			if (i + 2*stepSize >= size && size % numThreads != 0) {
 				end = size;
 				exit = true;
 			} else {
 				end = Math.min(i+stepSize, size);
 			}
+			//Give this section to a thread, and start it 
 			Thread t = blurPart(copy, i, end, blurSize);
 			t.start();
 			threads[count++] = t;
 		}
-
+		
+		//join each thread back to main thread
 		for (int x = 0; x < numThreads; x++) {
 			threads[x].join();
 		}
 		return copy;
-
 	}
 	
-	
-	public Thread blurPart(int [] [] image, int start, int end,  int blurSize) {
+	// Returns a handle to a thread with a runnable to blur 
+	// using a blur radius of blurSize
+	// the section of the image on the range [start:end)
+	private Thread blurPart(int [] [] image, int start, int end,  int blurSize) {
 		Thread t = new Thread(new Runnable() {
 		
 			@Override
@@ -64,6 +72,9 @@ public class MultiBlur {
 		return t;
 	}
 
+	// Returns the blurred color of the pixel at row, col of 
+	// using the class level image. Does not change the actual
+	// pixel, just returns the new value as a Color object
 	private Color blurPixel(int row, int col, int blurSize) {
 		
 		if (blurSize >= Math.min(image.length, image[0].length)) {
@@ -75,10 +86,7 @@ public class MultiBlur {
 		int bottom = Math.min(row+blurSize+1, image.length);
 		int top = Math.max(row-blurSize,0);
 		
-		int redSum = 0;
-		int blueSum = 0;
-		int greenSum = 0;
-		int count = 0;
+		int redSum = 0, blueSum = 0, greenSum = 0, count = 0;
 		
 		for (int j = top; j < bottom; j++) {
 			for (int i = left; i < right; i++) {
@@ -89,11 +97,6 @@ public class MultiBlur {
 				count++;
 			}
 		}
-		
-		int blue = blueSum/count;
-		int green = greenSum/count;
-		int red = redSum/count;
-		return new Color(red, green, blue);
+		return new Color(redSum/count, greenSum/count, blueSum/count);
 	}
-
 }
